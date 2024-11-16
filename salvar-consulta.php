c4ee36662f5ff97076a69c75b9e7033f42ebf82b
giovannaarrais <giovannaarrais13@gmail.com>
2024-11-15 21:42:09 -0300

    home +imgs

--- salvar-consulta.php
+++ salvar-consulta.php
@@ -1,21 +1,15 @@
 <?php
 	switch ($_REQUEST["acao"]) {
 		case 'cadastrar':
-			$paciente = $_POST['paciente_id_paciente'];
-			$medico = $_POST['medico_id_medico'];
 			$data = $_POST['data_consulta'];
             $horario = $_POST['hora_consulta'];
 			$descricao = $_POST['descricao_consulta'];
 
 			$sql = "INSERT INTO consulta (
-						paciente_id_paciente,
-						medico_id_medico,
                         data_consulta,
 						hora_consulta,
 						descricao_consulta)
 					VALUES (
-						'{$paciente}',
-						'{$medico}',
                         '{$data}',
 						'{$horario}',
 						'{$descricao}')";
@@ -33,15 +27,14 @@
 		
 		case 'editar':
 			$sql = "UPDATE consulta SET
-						paciente_id_paciente='".$_POST['paciente_id_paciente']."',
-						medico_id_medico='".$_POST['medico_id_medico']."',
-                        data_consulta='".$_POST['data_consulta']."',
+						data_consulta='".$_POST['data_consulta']."',
 						hora_consulta='".$_POST['hora_consulta']."',
 						descricao_consulta='".$_POST['descricao_consulta']."'
 					WHERE
-						id_consulta = ".$_POST['id_consulta'];
+						id_consulta  = ".$_POST['id_consulta']; 
 
 			$res = $conn->query($sql);
+
 
 			if($res==true){
 				print "<script>alert('Editou com sucesso!');</script>";
